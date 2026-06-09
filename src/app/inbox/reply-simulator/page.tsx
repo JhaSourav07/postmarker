0be12e8879a319570eecfcf885f54283011d23c3
Sourav Jha <souravkjha2007@gmail.com>
2026-06-09 11:26:38 +0530

    feat: add reply simulator page to inbox module

---
+++ src/app/inbox/reply-simulator/page.tsx
@@ -0,0 +1,9 @@
+import ReplySimulatorClient from "../../../components/conversation/ReplySimulatorClient";
+
+export default function ReplySimulatorPage() {
+  return (
+    <div className="min-h-[calc(100vh-112px)] bg-[#0B0D10] text-[#F8F8F8] flex items-center justify-center">
+      <ReplySimulatorClient />
+    </div>
+  );
+}
